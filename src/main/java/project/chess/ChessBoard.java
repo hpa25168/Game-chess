f161acce764cf8068fdd129845103b511859e184
hpa25168 <hpa25168@gmail.com>
2020-12-21 22:59:01 -0500

    add ChessBoard Class

--- src/main/java/project/chess/ChessBoard.java
+++ src/main/java/project/chess/ChessBoard.java
@@ -1,2 +1,17 @@
-package project.chess;public class ChessBoard {
+package project.chess;
+
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+
+public class ChessBoard extends VBox {
+    GridPane gameBoard;
+    public ChessBoard() {
+        super(10);
+        this.getChildren().add(board());
+
+    }
+    public GridPane board() {
+        gameBoard = new GridPane();
+        return gameBoard;
+    }
 }
