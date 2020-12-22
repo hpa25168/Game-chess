c2831d6778debd37538a4485b41894505da2e261
hpa25168 <hpa25168@gmail.com>
2020-12-21 22:58:29 -0500

    added Button

--- src/main/java/project/chess/ChessApp.java
+++ src/main/java/project/chess/ChessApp.java
@@ -1,7 +1,10 @@
 package project.chess;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -10,11 +13,18 @@ public class ChessApp extends Application {
     VBox Layout; //for Game Switcher
     @Override
     public void start(Stage primaryStage) {
+        Layout = new VBox();
+        Button chessButton = new Button("Chess");
         ImageView chessImage = new ImageView("https://www.karacterbox.com/media/catalog/product/cache/9d08971813a040f8f96067a40f75c615/2/9/29eab83d85d79e0d966a6cec840fdc1a_f_6034010_1.jpg");
-        Layout.getChildren().addAll(chessImage);
+        chessImage.setFitHeight(300.0);
+        chessImage.setPreserveRatio(true);
+        Layout.getChildren().addAll(chessImage,chessButton);
+        Layout.setAlignment(Pos.CENTER);
         Scene mainScene = new Scene(Layout);
         primaryStage.setScene(mainScene);
         primaryStage.setTitle("Chess");
         primaryStage.show();
+    }
+    public void chessAction(ActionEvent event) {
     }
 }
---
+++ src/main/java/project/chess/ChessBoard.java
@@ -0,0 +1,2 @@
+package project.chess;public class ChessBoard {
+}
